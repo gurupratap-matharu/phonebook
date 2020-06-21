f9f16fe1c12a8c98933c1c1a4a03e3993f75f340
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-06-21 17:02:23 -0300

    initializing array here

--- src/index.js
+++ src/index.js
@@ -2,4 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '123-456-789',
+  },
+  {
+    id: 2,
+    name: 'Princy',
+    number: '123-666-789',
+  }, {
+    id: 3,
+    name: 'Nancy',
+    number: '999-456-789',
+  }, {
+    id: 4,
+    name: 'mummy',
+    number: '123-555-789',
+  },
+]
+ReactDOM.render(<App persons={persons} />, document.getElementById('root'))
